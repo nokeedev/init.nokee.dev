ed514e63f7401e0ab8ac632bd632a96a498a21b8
Daniel Lacasse <daniel@nokee.dev>
2021-02-09 19:45:13 -0500

    Remove unused test

--- src/test/groovy/dev/nokee/init/internal/NokeeInitBuildListenerTest.groovy
+++
@@ -1,9 +0,0 @@
-package dev.nokee.init.internal
-
-import spock.lang.Specification
-import spock.lang.Subject
-
-@Subject(NokeeInitBuildListener)
-class NokeeInitBuildListenerTest extends Specification {
-	// TODO:
-}
