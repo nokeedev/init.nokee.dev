d54b4b4e87b5d64d95a3e48723d1b4448f9216ca
Daniel Lacasse <daniel@nokee.dev>
2021-02-10 09:39:33 -0500

    Use IOUtils.copy implementation instead of Files.copy

--- src/main/java/dev/nokee/init/internal/utils/IOUtils.java
+++ src/main/java/dev/nokee/init/internal/utils/IOUtils.java
@@ -2,6 +2,8 @@ package dev.nokee.init.internal.utils;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public final class IOUtils {
 	public static void closeQuietly(Closeable closeable) {
@@ -10,5 +12,24 @@ public final class IOUtils {
 		} catch (IOException e) {
 			// ignore
 		}
+	}
+
+	private static final int EOF = -1;
+	private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
+	public static int copy(InputStream input, OutputStream output) throws IOException {
+		// copyLarge
+		byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+		long count = 0;
+		int n = 0;
+		while (EOF != (n = input.read(buffer))) {
+			output.write(buffer, 0, n);
+			count += n;
+		}
+
+		// copy
+		if (count > Integer.MAX_VALUE) {
+			return -1;
+		}
+		return (int) count;
 	}
 }
--- src/main/java/dev/nokee/init/internal/wrapper/GradleWrapperActionNokeeInitScriptWriter.java
+++ src/main/java/dev/nokee/init/internal/wrapper/GradleWrapperActionNokeeInitScriptWriter.java
@@ -1,18 +1,17 @@
 package dev.nokee.init.internal.wrapper;
 
+import lombok.val;
 import org.gradle.api.Action;
 import org.gradle.api.Task;
 import org.gradle.api.provider.Provider;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UncheckedIOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.Objects;
 
 import static dev.nokee.init.internal.utils.IOUtils.closeQuietly;
+import static dev.nokee.init.internal.utils.IOUtils.copy;
 
 public final class GradleWrapperActionNokeeInitScriptWriter implements Action<Task> {
 	private final Provider<File> nokeeInitScriptFileProvider;
@@ -26,9 +25,8 @@ public final class GradleWrapperActionNokeeInitScriptWriter implements Action<Ta
 		InputStream inStream = this.getClass().getResourceAsStream("nokee.init.gradle");
 		Objects.requireNonNull(inStream, "Could not find the Nokee init script inside the classpath.");
 
-		try {
-			Files.copy(inStream, nokeeInitScriptFileProvider.get().toPath(), StandardCopyOption.REPLACE_EXISTING);
-			nokeeInitScriptFileProvider.get().setExecutable(false, false);
+		try (val outStream = new FileOutputStream(nokeeInitScriptFileProvider.get())) {
+			copy(inStream, outStream);
 		} catch (IOException e) {
 			throw new UncheckedIOException(String.format("Could not write Nokee init script file at '%s'.", nokeeInitScriptFileProvider.get().getAbsolutePath()), e);
 		} finally {
