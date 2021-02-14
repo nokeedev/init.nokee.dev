8c1dac3230537293f52f8a9960ebb814c7bd4f6d
Daniel Lacasse <daniel@nokee.dev>
2021-02-13 22:29:40 -0500

    WIP

--- src/functionalTest/groovy/dev/nokee/init/GradleWrapperLenientPatchingFunctionalTest.groovy
+++ src/functionalTest/groovy/dev/nokee/init/GradleWrapperLenientPatchingFunctionalTest.groovy
@@ -2,7 +2,11 @@ package dev.nokee.init
 
 import dev.gradleplugins.integtests.fixtures.AbstractGradleSpecification
 
+import java.nio.file.FileVisitResult
+import java.nio.file.FileVisitor
 import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.attribute.BasicFileAttributes
 
 import static dev.gradleplugins.runnerkit.GradleExecutor.gradleWrapper
 import static dev.gradleplugins.runnerkit.GradleRunner.create
@@ -22,6 +26,28 @@ class GradleWrapperLenientPatchingFunctionalTest extends AbstractGradleSpecifica
 		file('gradle/nokee.init.gradle').delete()
 		println Files.exists(file('gradle/nokee.init.gradle').toPath())
 		println(new String(Files.readAllBytes(file('gradle/nokee.init.gradle').toPath())))
+		Files.walkFileTree(testDirectory, new FileVisitor<Path>() {
+			@Override
+			FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+				return FileVisitResult.CONTINUE
+			}
+
+			@Override
+			FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				println(file)
+				return FileVisitResult.CONTINUE
+			}
+
+			@Override
+			FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+				return FileVisitResult.CONTINUE
+			}
+
+			@Override
+			FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+				return FileVisitResult.CONTINUE
+			}
+		})
 
 		expect:
 		def result = create(gradleWrapper()).inDirectory(testDirectory).withTasks('tasks').build()
